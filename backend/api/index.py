e72b3ef072103da95080e272ef08aed3b9ae4115
siddadatrao <sidd.adatrao@gmail.com>
2024-09-30 08:52:32 -0700

    Fixing deployment of backend issue

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class PodcastRequest(BaseModel):
     podcast_url: str
